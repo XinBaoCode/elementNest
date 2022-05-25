316262853dd0c699d6f650aba3f7e6b03204c597
XinBaoCode <1392368311@qq.com>
2022-05-25 23:46:54 +0800

    init Project

--- src/ele-bean/dto/update-ele-bean.dto.ts
+++ src/ele-bean/dto/update-ele-bean.dto.ts
@@ -1,6 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEleBeanDto } from './create-ele-bean.dto';
-
 export class UpdateEleBeanDto extends PartialType(CreateEleBeanDto) {
   qq?: number;
   eleCookie?: string;
